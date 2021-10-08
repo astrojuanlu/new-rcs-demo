84ab5b358a40be43b25f38026ddb8788fc194938
Juan Luis Cano Rodríguez <hello@juanlu.space>
2021-10-08 12:47:32 +0000

    Add web application

--- rcs.py
+++ rcs.py
@@ -1,6 +1,9 @@
+from flask import Flask, request
 from numpy import cos
 
 G_MARS = 3.72  # m / s2
+
+app = Flask(__name__)
 
 
 def kinetic_energy(velocity):
@@ -13,3 +16,22 @@ def potential_energy(altitude, gravity=G_MARS):
 
 def edm_altitude(rda_range, pitch):
     return rda_range * cos(pitch)
+
+
+@app.route("/")
+def home():
+    return "System OK"
+
+
+@app.route("/altitude")
+def compute_altitude():
+    rda_range = float(request.args["rda_range_m"])
+    pitch = float(request.args["pitch_rad"])
+
+    altitude = edm_altitude(rda_range, pitch)
+
+    return f"Altitude: {altitude:.2f}"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8899)
